a20697b1bcbaa0454cdf3c7a7c68d7ded652d0ad
aguadodev <aguado@edu.xunta.es>
2022-03-10 23:58:21 +0100

    primeras correcciones

--- src/test/java/rede/RedeTest.java
+++ src/test/java/rede/RedeTest.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
+
 package rede;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author cosme
- */
 public class RedeTest {
     
     public RedeTest() {
